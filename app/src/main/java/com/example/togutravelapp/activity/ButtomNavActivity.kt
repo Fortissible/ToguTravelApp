939ef4191bce0f3c1862fd32e58ac930119cb283
wildanfajri1alfarabi <78713685+wildanfajri1alfarabi@users.noreply.github.com>
2022-06-03 07:29:27 +0700

    add bottom nav and big changes in UI Component
    changing from activity to fragment for implementing bottom nav

---
+++ app/src/main/java/com/example/togutravelapp/activity/ButtomNavActivity.kt
@@ -0,0 +1,35 @@
+package com.example.togutravelapp.activity
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.togutravelapp.activity.databinding.ActivityButtomNavBinding
+
+class ButtomNavActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityButtomNavBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityButtomNavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_buttom_nav)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
