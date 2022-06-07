2e6c1454c3e4bd12513b1ea456f21773335d4167
fadzas <79698592+fadzas@users.noreply.github.com>
2022-06-07 21:12:46 +0700

    UI Profile

---
+++ app/src/main/java/com/example/togutravelapp/activity/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.togutravelapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.togutravelapp.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
